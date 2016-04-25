f883d3762bff8f66d2931ea5ea13a924b2353fa6
rcebulko <ryan@cebulko.com>
2016-04-25 03:57:53 -0400

    Implemented persistent storage

--- storage.py
+++ storage.py
@@ -1,3 +1,4 @@
+import os
 from logging import getLogger
 
 log = getLogger(__name__)
@@ -7,23 +8,42 @@ class Storage:
         self.store = {}
         self.file_dir = file_dir
 
-        self.store["2ea970ff63aec5d7a014ca6447ec743d3ba37450b85ebdcbb582b089b0194fa2"] = b"\xdf"
+        self.set(
+            "2ea970ff63aec5d7a014ca6447ec743d3ba37450b85ebdcbb582b089b0194fa2",
+            b"\xdf")
+
+    def path_for_key(self, key):
+        return os.path.join(self.file_dir, key)
 
     def keys(self):
-        return self.store.keys()
+        return os.listdir(self.file_dir)
 
     def has(self, key):
-        return key in self.store
+        return os.path.exists(self.path_for_key(key))
 
     def get(self, key):
-        if key in self.store:
-            return self.store[key]
-
-        return None
+        try:
+            with open(self.path_for_key(key), "rb") as file:
+                return file.read()
+        except:
+            return None
 
     def set(self, key, value):
-        log.info("Stored %s" % key)
-        self.store[key] = value
+        try:
+            with open(self.path_for_key(key), "wb") as file:
+                file.write(value)
+                return True
+        except:
+            return False
 
     def clear(self, key):
-        del self.store[key]
+        os.unlink(self.path_for_key(key))
+
+if __name__ == "__main__":
+    storage = Storage("dht_store/")
+    print(storage.has("fake_keys"))
+    print(storage.get("byte_keys"))
+    print(storage.set("testing", "value"))
+    storage.clear("2ea970ff63aec5d7a014ca6447ec743d3ba37450b85ebdcbb582b089b0194fa2")
+    print(storage.keys())
+    storage.clear("testing")
